dba002c7fa202f1a755045728362d158b6174ee0
liacociorva <33039032+liacociorva@users.noreply.github.com>
2019-05-25 16:58:16 -0700

    Create arrays.c

---
+++ cdemo/arrays.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+void arrayAdd(int*, int, int);
+
+int main() 
+{
+  int arr [100];
+  for (int i = 0; i < 100; ++i)
+  {
+    arr[i] = i * i;
+  }
+
+  for (int i = 0; i < 100; ++i)
+  {
+    printf ("%d > %d\n" , i, arr[i]);
+  }
+  arrayAdd(arr, 100, 5);
+  printf ("adding 5: \n");
+  for (int i = 0; i < 100; ++i)
+  {
+    printf ("%d > %d\n" , i, arr[i]);
+  }
+}
+void arrayAdd(int* inputArray, int s, int n)
+{
+  for (int i = 0; i < s; ++i)
+  {
+    inputArray[i]+=n;
+  }
+}
