585b4e7b0d3401256a323e4234dc236ae827d858
liacociorva <33039032+liacociorva@users.noreply.github.com>
2019-06-03 21:27:43 -0700

    Update mastermind.c

--- finalproject/mastermind.c
+++ finalproject/mastermind.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 void printScore(int*);
+void compareArrays(int*, int*, int*);
 
 
-
-int main(void) {
-int score[2];
-score[0] = 2;
-score[1] = 3;
-printScore(&score[0]);
+int main(void) 
+{
+  int score[2];
+  int secretCode[5];
+  int guessCode[5];
+  secretCode[0]=1;
+  secretCode[1]=3;
+  secretCode[2]=7;
+  secretCode[3]=2;
+  secretCode[4]=9;
+  guessCode[0]=1;
+  guessCode[1]=0;
+  guessCode[2]=9;
+  guessCode[3]=2;
+  guessCode[4]=7;
+  printScore(&score[0]);
+  compareArrays(secretCode, guessCode, score);
+  printScore(&score[0]);
   return 0;
 }
 
@@ -15,3 +28,36 @@ void printScore (int* a)
 {
   printf("Exact matches: %d, Inexact matches: %d\n", a[0], a[1]);
 }
+
+void compareArrays (int* inputArray1, int* inputArray2, int* scoreArray)
+{
+  int countMatch=0;
+  for (int index=0; index<5; index++)
+  {
+    if (inputArray1[index] == inputArray2[index])
+    {
+      countMatch++;
+    }
+  }
+  scoreArray[0]=countMatch;
+
+  int countOverall=0;
+  for (int index1=0; index1<5; index1++)
+  {
+    for (int index2=0; index2<5; index2++)
+    {
+      if (inputArray1[index1] == inputArray2[index2])
+      {
+        countOverall++;
+      }
+    }
+  }
+  int countMisses = countOverall - countMatch;
+
+  scoreArray[1]=countMisses;
+  
+}
+
+
+
+
